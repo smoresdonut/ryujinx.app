d3da80b470359db2bd414c74e35e9acb5284e47c
donut <69540471+smoresdonut@users.noreply.github.com>
2024-11-06 03:04:43 +0100

    add cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import cloudflare from "@sveltejs/adapter-cloudflare";
 import { sveltePreprocess } from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
+		adapter: cloudflare({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
